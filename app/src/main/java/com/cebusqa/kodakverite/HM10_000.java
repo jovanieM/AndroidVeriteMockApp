cded9ace3489cb97aad68ffa45e93792db26dba6
Miravy <mmolo@funai-cebu.com.ph>
2016-06-24 13:34:10 +0800

    modified back button function

--- app/src/main/java/com/cebusqa/kodakverite/HM10_000.java
+++ app/src/main/java/com/cebusqa/kodakverite/HM10_000.java
@@ -29,13 +29,12 @@ public class HM10_000 extends AppCompatActivity {
         scandocument =(ImageButton)findViewById(R.id.scandocument);
         photoprint =(ImageButton)findViewById(R.id.photoprint);
         setting_icon =(ImageButton)findViewById(R.id.setting_icon);
-<<<<<<< HEAD
+
         printer = (ImageButton)findViewById(R.id.printer);
         search_icon= (ImageButton)findViewById(R.id.search_icon);
         printer_name= (ImageButton)findViewById(R.id.printer_name);
 
-=======
->>>>>>> 29478d56205b44dacdf76e7927af7629017a6ac3
+
 
         inklevel.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
