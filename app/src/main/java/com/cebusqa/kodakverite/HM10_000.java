630da5f63c0dcf1a01013c8edf0098f0e7a8d270
Miravy <mmolo@funai-cebu.com.ph>
2016-06-24 14:58:55 +0800

    modified back button function

--- app/src/main/java/com/cebusqa/kodakverite/HM10_000.java
+++ app/src/main/java/com/cebusqa/kodakverite/HM10_000.java
@@ -3,6 +3,7 @@ package com.cebusqa.kodakverite;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,6 +16,8 @@ public class HM10_000 extends AppCompatActivity {
     ImageButton inklevel, copy, scanphoto, scandocument, photoprint, ecomode, setting_icon, printer, search_icon, printer_name;
     private ProgressDialog progressBar;
     private int progressBarStatus = 0;
+    private int currentImage = 0;
+    int[] images = {R.mipmap.ecomode_off, R.mipmap.ecomode1, R.mipmap.ecomode2};
 
 
     @Override
@@ -29,11 +32,9 @@ public class HM10_000 extends AppCompatActivity {
         scandocument =(ImageButton)findViewById(R.id.scandocument);
         photoprint =(ImageButton)findViewById(R.id.photoprint);
         setting_icon =(ImageButton)findViewById(R.id.setting_icon);
-
         printer = (ImageButton)findViewById(R.id.printer);
         search_icon= (ImageButton)findViewById(R.id.search_icon);
         printer_name= (ImageButton)findViewById(R.id.printer_name);
-
 
 
         inklevel.setOnClickListener(new View.OnClickListener(){
@@ -65,18 +66,25 @@ public class HM10_000 extends AppCompatActivity {
         ecomode.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
 
-                progressBar = new ProgressDialog(v.getContext());
-                progressBar.setCancelable(true);
-                progressBar.setMessage("Confirming ...");
-                progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                progressBar.show();
-        //        progressBar.setProgress(10);
-                progressBar.setMax(10);
 
-        //        progressBarStatus = 10;
-                progressBar.dismiss();
+                        //Increase Counter to move to next Image
+                        currentImage++;
+                        currentImage = currentImage % images.length;
+
+                RingDialog ringDialog = new RingDialog(HM10_000.this, "", "Confirming...", true);
+                ringDialog.run();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ecomode.setImageResource(images[currentImage]);
+                    }
+                }, 4000);
+
 
             }
+
+
         });
 
 
