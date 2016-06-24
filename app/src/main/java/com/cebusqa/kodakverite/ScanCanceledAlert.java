51f459f342f200ca35cecfe0ee8caf1d9b03b347
Miravy <mmolo@funai-cebu.com.ph>
2016-06-24 08:46:13 +0800

    add

---
+++ app/src/main/java/com/cebusqa/kodakverite/ScanCanceledAlert.java
@@ -0,0 +1,40 @@
+package com.cebusqa.kodakverite;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Cebu SQA on 23/06/2016.
+ */
+public class ScanCanceledAlert extends DialogFragment {
+
+    public ScanCanceledAlert newInstance(String title) {
+        ScanCanceledAlert frag = new ScanCanceledAlert();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String title = getArguments().getString("title");
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(title)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                //((SP_000)getActivity()).test2 = true;
+                                dialog.dismiss();
+
+                            }
+                        }
+                )
+
+                .create();
+    }
+}
