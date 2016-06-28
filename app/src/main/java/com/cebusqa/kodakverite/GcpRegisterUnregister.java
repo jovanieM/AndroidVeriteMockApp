cf7c990744e91d7cdb03cf4eea8933c67cfe996c
jmolas <jmolas@funai-cebu.com.ph>
2016-06-28 08:36:06 +0800

    modified

--- app/src/main/java/com/cebusqa/kodakverite/GcpRegisterUnregister.java
+++ app/src/main/java/com/cebusqa/kodakverite/GcpRegisterUnregister.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class GcpRegisterUnregister extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gcp_registration);
+        final String [] status = getResources().getStringArray(R.array.gcp_reg_status);
         tvDescription = (TextView) findViewById(R.id.description);
         final DialogFragment gcpStatusLoadingDialog2 = GcpStatusLoadingDialog.newInstance("GCP Status Loading...");
         gcpStatusLoadingDialog2.show(getFragmentManager(),"my tag");
@@ -37,6 +39,8 @@ public class GcpRegisterUnregister extends Activity {
                     finish();
 
 
+                }else{
+                    regUnreg.setText(status[1]);
                 }
             }
         }).start();
