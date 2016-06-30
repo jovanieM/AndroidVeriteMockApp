b641b3df16644ccdf84923447b0d3fbda0b0f0ba
anarte <anarte@funai-cebu.com.ph>
2016-06-30 13:22:43 +0800

    modified: UpdatePrinterSoftware.java WS00_000.java software_update.xml update_printer_software.xml

--- app/src/main/java/com/cebusqa/kodakverite/WS00_000.java
+++ app/src/main/java/com/cebusqa/kodakverite/WS00_000.java
@@ -28,13 +28,7 @@ public class WS00_000 extends Activity {
         context=this;
         prgItems = getResources().getStringArray(R.array.wifi_setup);
 
-        back = (Button)findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+
 
         lvWifiSetupItems = (ListView) findViewById(R.id.lv_wifi_setup);
         lvWifiSetupItems.setAdapter(new CustomAdapter(this, prgItems));
