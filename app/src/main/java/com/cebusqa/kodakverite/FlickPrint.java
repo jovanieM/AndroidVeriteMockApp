a45ec84fddb6b3729f27a6e05bb65280af15372d
Jovanie <jmolas@funai-cebu.com.ph>
2016-08-12 17:36:37 +0800

    modified

--- app/src/main/java/com/cebusqa/kodakverite/FlickPrint.java
+++ app/src/main/java/com/cebusqa/kodakverite/FlickPrint.java
@@ -2,23 +2,14 @@ package com.cebusqa.kodakverite;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.view.GestureDetectorCompat;
-import android.transition.TransitionManager;
-import android.view.DragEvent;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
@@ -52,7 +43,8 @@ public class FlickPrint extends Activity implements CompoundButton.OnCheckedChan
     RelativeLayout inst;
     ImageView settingFlick;
     Button backIcon;
-
+    Intent intent;
+    Intent chooser;
 
 
     @Override
@@ -92,53 +84,55 @@ public class FlickPrint extends Activity implements CompoundButton.OnCheckedChan
 
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
         imDisplay.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                mDetector.onTouchEvent(event);
-                switch (event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        moving = true;
-                        prevY = event.getRawY();
-                        displayYcoor = imDisplay.getY();
+                                         @Override
+                                         public boolean onTouch(View v, MotionEvent event) {
+                                             mDetector.onTouchEvent(event);
+                                             switch (event.getAction()) {
+                                                 case MotionEvent.ACTION_DOWN:
+                                                     moving = true;
+                                                     prevY = event.getRawY();
+                                                     displayYcoor = imDisplay.getY();
 
-                        //Toast.makeText(getApplicationContext(), String.valueOf(event.getRawY()), Toast.LENGTH_SHORT).show();
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        if (moving){
-                            y = event.getRawY() - prevY;
+                                                     //Toast.makeText(getApplicationContext(), String.valueOf(event.getRawY()), Toast.LENGTH_SHORT).show();
+                                                     break;
+                                                 case MotionEvent.ACTION_MOVE:
+                                                     if (moving) {
+                                                         y = event.getRawY() - prevY;
 
-                            imDisplay.setY(displayYcoor + y);
-                            last = imDisplay.getY();
+                                                         imDisplay.setY(displayYcoor + y);
+                                                         last = imDisplay.getY();
 
-                        }
-                        break;
-                    case MotionEvent.ACTION_UP:
+                                                     }
+                                                     break;
+                                                 case MotionEvent.ACTION_UP:
 
-                        if(prevY - event.getRawY() > 200){
-                            imDisplay.setY(0-imDisplay.getHeight());
-                            startActivity(new Intent(getApplication(), FlickPrintOut.class));
-                            finish();
-                        }else{
-                            imDisplay.setY(displayYcoor);
-                        }
-                           // height = imDisplay.getY();
-                           // Toast.makeText(getApplicationContext(), String.valueOf(height), Toast.LENGTH_SHORT).show();
+                                                     if (prevY - event.getRawY() > 200) {
+                                                         imDisplay.setY(0 - imDisplay.getHeight());
+
+                                                         startActivity(new Intent(getApplication(), FlickPrintOut.class));
+                                                         finish();
+                                                     } else {
+                                                         imDisplay.setY(displayYcoor);
+                                                     }
+                                                     // height = imDisplay.getY();
+                                                     // Toast.makeText(getApplicationContext(), String.valueOf(height), Toast.LENGTH_SHORT).show();
 //                        if(threshold){
 //                            startActivity(new Intent(getApplication(), PhotoScanMain.class));
 //                        }else{
 //
 //                        }
-                        break;
-                }
-                return true;
-            }
-        });
-        ImageLoader.getInstance().displayImage("file:///"+fullImage, imDisplay, new DisplayImageOptions.Builder()
+                                                     break;
+                                             }
+                                             return true;
+                                         }
+                                     }
+        );
+
+        ImageLoader.getInstance().displayImage("file:///" + fullImage, imDisplay, new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
                 .displayer(new SimpleBitmapDisplayer())
                 .build());
-
 
 
     }
@@ -151,14 +145,14 @@ public class FlickPrint extends Activity implements CompoundButton.OnCheckedChan
      */
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if(isChecked){
+        if (isChecked) {
 //            flickImage.setVisibility(View.VISIBLE);
 //            tv.setVisibility(View.VISIBLE);
 //            flickMessage.setVisibility(View.VISIBLE);
 //            relativeLayout.setVisibility(View.VISIBLE);
             inst.setVisibility(View.VISIBLE);
 
-        }else{
+        } else {
 //            flickImage.setVisibility(View.GONE);
 //            tv.setVisibility(View.GONE);
 //            flickMessage.setVisibility(View.GONE);
@@ -169,7 +163,7 @@ public class FlickPrint extends Activity implements CompoundButton.OnCheckedChan
     }
 
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
 
         @Override
         public boolean onDown(MotionEvent e) {
@@ -178,17 +172,14 @@ public class FlickPrint extends Activity implements CompoundButton.OnCheckedChan
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            if (e1.getY() - e2.getY() > 100 && Math.abs(velocityY) > 50)
-            {
+            if (e1.getY() - e2.getY() > 100 && Math.abs(velocityY) > 50) {
                 threshold = true;
                 //Toast.makeText(getApplicationContext(), "You have swipped top side", Toast.LENGTH_SHORT).show();
-              //  startActivity(new Intent(getApplication(), PhotoScanMain.class));
+                //  startActivity(new Intent(getApplication(), PhotoScanMain.class));
            /* Code that you want to do on swiping left side*/
                 return true;
-            }
-            else if (e2.getY() - e1.getY() > 50 && Math.abs(velocityY) > 50)
-            {
-                Toast.makeText(getApplicationContext(), "You have swipped right side", Toast.LENGTH_SHORT).show();
+            } else if (e2.getY() - e1.getY() > 50 && Math.abs(velocityY) > 50) {
+                //Toast.makeText(getApplicationContext(), "You have swipped right side", Toast.LENGTH_SHORT).show();
           /* Code that you want to do on swiping right side*/
                 return false;
             }
