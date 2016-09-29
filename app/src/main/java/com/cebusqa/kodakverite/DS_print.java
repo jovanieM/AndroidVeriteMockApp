640dda93c279f04ba6bf0621bea0125688082a72
Miravy <mmolo@funai-cebu.com.ph>
2016-09-29 16:38:34 +0800

    commit

--- app/src/main/java/com/cebusqa/kodakverite/DS_print.java
+++ app/src/main/java/com/cebusqa/kodakverite/DS_print.java
@@ -1,28 +1,19 @@
 package com.cebusqa.kodakverite;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 
 public class DS_print extends FragmentActivity implements View.OnClickListener {
 
-    //public ImageButton quick, detail;
+    public ImageButton quick, detail;
     public Button back;
-    TextView quick, detail;
-    RelativeLayout quick_detail;
 
 
     @Override
@@ -30,23 +21,21 @@ public class DS_print extends FragmentActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ds_print);
 
-        //quick = (ImageButton) findViewById(R.id.quick);
-        //detail = (ImageButton) findViewById(R.id.detail);
-        quick = (TextView) findViewById(R.id.tv_quick);
-        detail = (TextView) findViewById(R.id.tv_detail);
-        quick_detail = (RelativeLayout) findViewById(R.id.quick_detail);
+        quick = (ImageButton) findViewById(R.id.quick);
+        detail = (ImageButton) findViewById(R.id.detail);
         back = (Button)findViewById(R.id.back);
+
+
 
         back.setOnClickListener(new OnClickListener(){
             public void onClick(View v){
-                startActivity(new Intent(DS_print.this, DS10_000.class));
                 finish();
             }
         });
 
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        final FragmentQuickPrint quick1 = new FragmentQuickPrint();
+        FragmentQuickPrint quick1 = new FragmentQuickPrint();
         ft.replace(R.id.view, quick1);
         ft.addToBackStack(null);
         ft.commit();
@@ -55,12 +44,8 @@ public class DS_print extends FragmentActivity implements View.OnClickListener {
 
             public void onClick (View v){
 
-                //detail.setImageResource(R.mipmap.detail_white);
-                //quick.setImageResource(R.mipmap.quick_yellow);
-
-                quick_detail.setBackgroundResource(R.mipmap.settingtab_left);
-                quick.setTextColor(getResources().getColor(R.color.yellow));
-                detail.setTextColor(getResources().getColor(R.color.gray));
+                detail.setImageResource(R.mipmap.detail_white);
+                quick.setImageResource(R.mipmap.quick_yellow);
 
                 FragmentManager fm = getFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
@@ -78,12 +63,10 @@ public class DS_print extends FragmentActivity implements View.OnClickListener {
 
             public void onClick (View v){
 
-                //quick.setImageResource(R.mipmap.quick_white);
-                //detail.setImageResource(R.mipmap.detail_yellow);
+                quick.setImageResource(R.mipmap.quick_white);
+                detail.setImageResource(R.mipmap.detail_yellow);
 
-                quick_detail.setBackgroundResource(R.mipmap.settingtab_right);
-                quick.setTextColor(getResources().getColor(R.color.gray));
-                detail.setTextColor(getResources().getColor(R.color.yellow));
+
 
                 FragmentManager fm = getFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
@@ -92,8 +75,11 @@ public class DS_print extends FragmentActivity implements View.OnClickListener {
                 ft.replace(R.id.view, details);
                 ft.addToBackStack(null);
                 ft.commit();
+
              }
         });
+
+
     }
 
     @Override
@@ -103,7 +89,6 @@ public class DS_print extends FragmentActivity implements View.OnClickListener {
 
     @Override
     public void onBackPressed() {
-        startActivity(new Intent(DS_print.this, DS10_000.class));
         finish();
         //super.onBackPressed();
     }
