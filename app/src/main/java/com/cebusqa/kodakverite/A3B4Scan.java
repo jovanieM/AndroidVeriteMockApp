9e7419b6082aa6e816db505bb9260e545607093d
Jovanie <jmolas@funai-cebu.com.ph>
2016-10-20 09:30:04 +0800

    added and modified
    app\src\main\res\drawable\world_map.jpg
    app\src\main\res\layout\a3b4_scan_result.xml
    app\src\main\res\layout\for_prott_screen2.xml
    app\src\main\AndroidManifest.xml
    app\src\main\java\com\cebusqa\kodakverite\A3B4Scan.java
    app\src\main\res\drawable\oval_background.xml
    app\src\main\res\layout\a3b4_scan.xml
    app\src\main\res\layout\a3b4_scan2.xml
    app\src\main\res\layout\for prott_screen2.xml
    app\src\main\res\layout\for_prott_screen1.xml

--- app/src/main/java/com/cebusqa/kodakverite/A3B4Scan.java
+++ app/src/main/java/com/cebusqa/kodakverite/A3B4Scan.java
@@ -1,8 +1,9 @@
 package com.cebusqa.kodakverite;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.view.View;
 
 /**
  * Created by jmolas on 19/10/2016.
@@ -12,6 +13,8 @@ public class A3B4Scan extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a3b4_scan);
+        setContentView(R.layout.a3b4_scan2);
+
     }
+
 }
